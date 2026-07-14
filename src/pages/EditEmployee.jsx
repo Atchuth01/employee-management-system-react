86be07a9147a03b2670337714e146c2f50d55db7
Atchuth01 <atchuth83@gmail.com>
2026-07-14 15:33:23 +0530

    refactor: centralize employee operations using custom hook

--- src/pages/EditEmployee.jsx
+++ src/pages/EditEmployee.jsx
@@ -1,9 +1,17 @@
+import { useParams } from "react-router-dom";
+import EmployeeForm from "../components/EmployeeForm";
+import useEmployees from "../hooks/useEmployees";
+
 function EditEmployee(){
+    const { id } = useParams();
+
+    const { getEmployeeById } = useEmployees();
+    const employee = getEmployeeById(id);
 
     return(
         <div className = "page">
             <h1>Edit Employee</h1>
-            <p>Edit emploee form will appear here</p>
+            <EmployeeForm employee = {employee} mode = "edit" />
         </div>
     )
 }
